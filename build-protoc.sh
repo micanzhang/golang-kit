d30ffe0facbb7d05ef940760f9e3f133f53863c1
Mican Zhang <mican.zhang@liulishuo.com>
2018-09-04 10:29:16 +0800

    update build protoc scripts

--- build-protoc.sh
+++ build-protoc.sh
@@ -5,6 +5,7 @@ set -e
 apk add --update autoconf automake libtool curl make g++ unzip
 git clone https://github.com/google/protobuf -b 3.6.x --depth 1
 cd ./protobuf
+git submodule update --init --recursive
 ./autogen.sh
 ./configure
 make
